181e6c2d03837dc487170c9c6387251647c5c030
Cargoland Project <cargolandproject@gmail.com>
2025-11-29 03:52:57 +0100

    updates to pubspec and build gradle

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -39,20 +39,23 @@ android {
         multiDexEnabled = true
     }
 
-    signingConfigs {
-        create("release") {
-            keyAlias = keystoreProperties["keyAlias"] as String?
-            keyPassword = keystoreProperties["keyPassword"] as String?
-            storeFile = keystoreProperties["storeFile"]?.let { file(it as String) }
-            storePassword = keystoreProperties["storePassword"] as String?
-        }
+   signingConfigs {
+    create("release") {
+        keyAlias = keystoreProperties["keyAlias"] as String?
+        keyPassword = keystoreProperties["keyPassword"] as String?
+        storeFile = keystoreProperties["storeFile"]?.let { file(it as String) }
+        storePassword = keystoreProperties["storePassword"] as String?
     }
+}
 
-    buildTypes {
-        getByName("release") {
-            signingConfig = signingConfigs.getByName("debug") // or "release" if you have real keystore
-        }
+buildTypes {
+    getByName("release") {
+        isMinifyEnabled = false
+        isShrinkResources = false
+        signingConfig = signingConfigs.getByName("release")
     }
+}
+
 }
 
 flutter {
